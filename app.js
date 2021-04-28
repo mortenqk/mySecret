6fcd41e87b9c9391c0250035b7f5ad6eedfb4e76
mortenqk <mortenqk@gmail.com>
2021-04-28 15:52:50 +0800

    added hash + salting

--- app.js
+++ app.js
@@ -3,9 +3,8 @@ const express = require ("express");
 const bodyParser = require ("body-parser");
 const ejs = require ("ejs");
 const mongoose = require ("mongoose");
-
-const md5 = require("md5")
-
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
 
@@ -27,7 +26,6 @@ const userSchema = new mongoose.Schema({
 });
 
 
-
 const User = new mongoose.model("User", userSchema);
 
 
@@ -46,9 +44,10 @@ app.get("/register", function(req,res){
 
 app.post("/register", function(req,res){
 
+bcrypt.hash(req.body.password,saltRounds,function(err,hash){
   const newUser = new User({
     email: req.body.username,
-    password: md5(req.body.password)
+    password: hash
   });
 
   newUser.save(function(err){
@@ -59,20 +58,24 @@ app.post("/register", function(req,res){
     }
   });
 });
+});
 
 
 app.post("/login", function(req,res){
     const username=req.body.username;
-    const password=md5(req.body.password);
+    const password=req.body.password;
 
 User.findOne({email:username}, function(err, foundUser){
   if (err){
     console.log(err)
   }else{
     if (foundUser){
-      if(foundUser.password===password){
-        res.render("secrets");
+      bcrypt.compare(password, foundUser.password, function(err, result){
+        if (result===true){
+          res.render("secrets");
+        }
       }
+    )
     }
   }
 })
